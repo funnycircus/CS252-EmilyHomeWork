70a0fba449bf46705d3e736ef8e9e3d0bde09c8d
unknown <funnycircus@SD2-funnycircus.ptc-nec.com.tw>
2015-04-27 20:55:50 +0800

    first commit by Emily

---
+++ ConsoleHomeWorkEmily/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleHomeWorkEmily
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("請輸入您的名字:");
+            string Name = Console.ReadLine();
+            Console.WriteLine("請問你是男是女?");
+            string Gender = Console.ReadLine();
+            Console.WriteLine(Name + "你好!!");
+            Console.WriteLine("我猜" + Name + "您一定是" + Gender + "!!");
+        }
+    }
+}
